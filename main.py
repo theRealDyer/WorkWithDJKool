d00c1395ded3fc0b5362c21c96b8bbcb9ebf8769
theRealDyer <54688154+theRealDyer@users.noreply.github.com>
2022-09-24 18:03:48 +0100

    Creation of main, debug, and settings py files
    Running main creates a black screen with debug text in top left corner.

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
-import pygame as pg
+# Hi Andrew :)
 
-pg.init()
+# Very basic setup for running a black screen at specified resolution.
 
-# testing
+import pygame, sys
+from settings import *
+from debug import *
 
-# more testing
+class Game:
+    def __init__(self):
+        
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('The Game')
+        self.clock = pygame.time.Clock()
+
+        
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            
+            self.screen.fill('black')
+
+            # run debugger
+            debug(pygame.mouse.get_pos())
+            debug(pygame.mouse.get_pressed(), 40)
+            
+
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+            
+  
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
+
+
+
+
---
+++ settings.py
@@ -0,0 +1,5 @@
+# game setup
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+FPS = 60
+
