f3e0092b5b26b4e779a3b56643d365abc309d7e6
theRealDyer <54688154+theRealDyer@users.noreply.github.com>
2023-12-14 15:01:43 +0000

    no idea. just pushing before deleting branch

--- debug.py
+++ debug.py
@@ -1,6 +1,18 @@
 # Useful debug function that lets you pop a text box up 
 # anywhere and analyse mouse_pos, key_press, etc
 
+# Update: To be implemented at a later date as per Andrew's suggestion:
+'''
+pygame.init() should only be called once (in the "Game" initialization), but this 
+would mean you'd have to delay the creation of the debug font until that happens. 
+This is where the controlled initialization of all game "systems", even those in 
+other modules, is important.
+We can model a debug system the same way we model our other systems and use it to
+provide all the additional overlay data we might need during development. That 
+way we can just "disable" (not enable) the system in production environments. We 
+can design this more later.
+'''
+
 import pygame
 pygame.init()
 font = pygame.font.Font(None, 30)
